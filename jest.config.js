db294901fa230dde2f5d97612a91c66cde36742f
gongbaodd@Inspiron_Ubuntu <gongbaodd@qq.com>
2020-03-27 16:13:14 +0800

    📚 Docs: Add badge

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,9 @@ module.exports = {
   verbose: true,
   transformIgnorePatterns: ["node_modules"],
   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  collectCoverageFrom: ["src/**/*.js", "src/**/*.svelte"],
+  coverageDirectory: "coverage",
+  coverageReporters: ["json", "lcov", "text-summary", "clover"],
+  clearMocks: true,
+  collectCoverage: true,
 };
